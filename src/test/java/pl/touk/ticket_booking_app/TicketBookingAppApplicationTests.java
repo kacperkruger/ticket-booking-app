20add3342e57f2671c412088bbc47f7e58f8e254
Kacper Kruger <kacper.kruger1@gmail.com>
2023-02-25 21:16:08 +0100

    init spring project

---
+++ src/test/java/pl/touk/ticket_booking_app/TicketBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.touk.ticket_booking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
